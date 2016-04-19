7958770e4edc279f27ee46df0c8e535be80093c5
Li Kaiwei <chnlkw@gmail.com>
2016-04-19 17:30:20 +0800

    basic test

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#export CXX=icpc
+#export CC=icc
+
+mkdir -p debug
+pushd debug
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+popd
+
+mkdir -p release
+pushd release
+cmake .. -DCMAKE_BUILD_TYPE=Release
+popd
---
+++ test/basic.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("OK\n");
+	return 0;
+}
