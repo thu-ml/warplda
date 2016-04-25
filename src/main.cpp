ee3cf7bdeb489aa82bd767b623e6a7737f764437
Li Kaiwei <chnlkw@gmail.com>
2016-04-25 11:31:39 +0800

    add gflags

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+#include <gflags/gflags.h>
+
+DEFINE_string(input, "", "Input file prefix");
+
 int main(int argc, char** argv)
 {
+	gflags::ParseCommandLineFlags(&argc, &argv, true);
+	printf("input %s\n", FLAGS_input.c_str());
+	return 0;
 }
